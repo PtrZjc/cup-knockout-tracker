ddce3edf435a1b5ca0645f3f359822f11a2f4446
Piotr Zajac <ptr.zjc@gmail.com>
2024-06-10 23:06:34 +0200

    Add red add match test for simple case

--- src/test/java/pl/zajacp/tracker/WorldCupTrackerTest.java
+++ src/test/java/pl/zajacp/tracker/WorldCupTrackerTest.java
@@ -3,23 +3,40 @@ package pl.zajacp.tracker;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import pl.zajacp.tracker.api.Match;
-import pl.zajacp.tracker.api.MatchStatus;
+import pl.zajacp.tracker.api.MatchResult;
 import pl.zajacp.tracker.api.Team;
-import pl.zajacp.tracker.api.TournamentStage;
 import pl.zajacp.tracker.api.exception.DuplicateTeamsException;
 import pl.zajacp.tracker.api.exception.InvalidTeamCountException;
+import pl.zajacp.tracker.api.exception.InvalidTeamOrderException;
+import pl.zajacp.tracker.api.exception.MatchNotFoundException;
 
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static pl.zajacp.tracker.api.MatchStatus.*;
-import static pl.zajacp.tracker.api.Team.*;
-import static pl.zajacp.tracker.api.TournamentStage.*;
+import static pl.zajacp.tracker.api.MatchStatus.FINISHED;
+import static pl.zajacp.tracker.api.MatchStatus.PLANNED;
+import static pl.zajacp.tracker.api.Team.ARGENTINA;
+import static pl.zajacp.tracker.api.Team.BELGIUM;
+import static pl.zajacp.tracker.api.Team.BRAZIL;
+import static pl.zajacp.tracker.api.Team.COLOMBIA;
+import static pl.zajacp.tracker.api.Team.CROATIA;
+import static pl.zajacp.tracker.api.Team.DENMARK;
+import static pl.zajacp.tracker.api.Team.ENGLAND;
+import static pl.zajacp.tracker.api.Team.FRANCE;
+import static pl.zajacp.tracker.api.Team.GERMANY;
+import static pl.zajacp.tracker.api.Team.ITALY;
+import static pl.zajacp.tracker.api.Team.MEXICO;
+import static pl.zajacp.tracker.api.Team.NETHERLANDS;
+import static pl.zajacp.tracker.api.Team.PORTUGAL;
+import static pl.zajacp.tracker.api.Team.SPAIN;
+import static pl.zajacp.tracker.api.Team.SWITZERLAND;
+import static pl.zajacp.tracker.api.Team.URUGUAY;
+import static pl.zajacp.tracker.api.TournamentStage.ROUND_OF_16;
 
 
-public class WorldCupTrackerTests {
+public class WorldCupTrackerTest {
 
     private final WorldCupTracker tracker = new WorldCupTrackerImpl();
 
@@ -33,6 +50,8 @@ public class WorldCupTrackerTests {
             DENMARK, SWITZERLAND,
             COLOMBIA, MEXICO
     );
+
+    private final static MatchResult REGULAR_MATCH_RESULT = new MatchResult(3, 2);
 
     @Test
     public void shouldInitializeWithSixteenTeams() {
@@ -74,20 +93,42 @@ public class WorldCupTrackerTests {
                 .hasMessageContaining("Duplicate teams found in the list. Each team must be unique.");
     }
 
-    @Disabled
     @Test
     public void shouldRecordValidMatchResult() {
         // given
+        tracker.startWorldCup(INITIAL_TEAMS);
+
         // when
+        var result = tracker.recordMatchResult(FRANCE, BRAZIL, REGULAR_MATCH_RESULT);
+
         // then
+        assertThat(tracker.getMatches())
+                .filteredOn(match -> (match.teamA() == FRANCE && match.teamB() == BRAZIL) ||
+                        (match.teamA() == BRAZIL && match.teamB() == FRANCE))
+                .containsExactly(new Match(FRANCE, BRAZIL, ROUND_OF_16, FINISHED, REGULAR_MATCH_RESULT));
     }
 
-    @Disabled
+
+    @Test
+    public void shouldThrowWrongTeamOrder() {
+        // given
+        tracker.startWorldCup(INITIAL_TEAMS);
+
+        // when then
+        assertThatThrownBy(() -> tracker.recordMatchResult(BRAZIL, FRANCE, REGULAR_MATCH_RESULT))
+                .isInstanceOf(InvalidTeamOrderException.class)
+                .hasMessageContaining("The match with provided teams exists, but they are in the wrong order: FRANCE is A and BRAZIL is B.");
+    }
+
     @Test
     public void shouldThrowMatchNotFoundExceptionForNonexistentMatch() {
         // given
-        // when
-        // then
+        tracker.startWorldCup(INITIAL_TEAMS);
+
+        // when then
+        assertThatThrownBy(() -> tracker.recordMatchResult(FRANCE, ARGENTINA, REGULAR_MATCH_RESULT))
+                .isInstanceOf(MatchNotFoundException.class)
+                .hasMessageContaining("No match found between home team FRANCE and away team BRAZIL");
     }
 
     @Disabled
